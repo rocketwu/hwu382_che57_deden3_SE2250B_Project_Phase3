05fcd4afaba0d1684aa57a110da38b1e15678302
che57 <35236747+che57@users.noreply.github.com>
2018-03-24 22:03:06 -0400

    blaster bullet with little problem
    the bullet doesn't turn in expect angle

--- Assets/__Script/Shooting.cs
+++ Assets/__Script/Shooting.cs
@@ -8,17 +8,36 @@ public class Shooting : MonoBehaviour {
 	public float bulletSpeed;
 	// Use this for initialization
 	void Awake () {
-		bulletSpeed = 2000f;
+		bulletSpeed = 50f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		//simpleShooting ();
+		blasterShooting ();	
+	}
+
+	private void simpleShooting(){
 		if (Input.GetKeyDown (KeyCode.Space)) {
 			_shootingPosition = this.GetComponent<Transform> ().position;
 			GameObject bullet = Instantiate<GameObject>(bulletPrefab);
 			Rigidbody rb = bullet.GetComponent<Rigidbody>();
 			bullet.transform.position = _shootingPosition;
-			rb.AddForce (Vector3.up * bulletSpeed);
-		}			
+			rb.velocity = Vector3.up * bulletSpeed;
+		}
+	}
+
+	private void blasterShooting(){
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			_shootingPosition = this.GetComponent<Transform> ().position;
+			for (float i = -1f; i <= 1f; i++) {
+				GameObject bullet = Instantiate<GameObject>(bulletPrefab);
+				Rigidbody rb = bullet.GetComponent<Rigidbody>();
+				bullet.transform.position = _shootingPosition;
+				bullet.transform.rotation = new Quaternion (0, 0, i * 0.6f, 0);
+				//rb.velocity = Vector3.up * bulletSpeed;
+				rb.velocity = new Vector3 (i*0.6f, 1f, 0) * bulletSpeed;
+			}
+		}
 	}
 }
