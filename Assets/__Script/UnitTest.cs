2bf486c984b0ec7d9e960a26882bd6f25ed4a669
hwu382@desktop <hwu382@uwo.ca>
2018-03-31 16:18:23 -0400

    level progression finished

---
+++ Assets/__Script/UnitTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UnitTest : MonoBehaviour {
+    public bool check = false;
+    public Text cutsceneTxt;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (check)
+        {
+            changeTxt();
+        }
+
+    }
+    public void changeTxt()
+    {
+        Color c = Random.ColorHSV();
+        cutsceneTxt.color = c;
+    }
+}
